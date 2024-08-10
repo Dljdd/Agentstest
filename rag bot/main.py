1c5f17c6de8d005fd30d66cefc4d01158a2bae3b
Dylan Moraes <dylanmoraesdljdd@gmail.com>
2024-08-10 22:48:56 +0530

    Update main.py

--- rag bot/main.py
+++ rag bot/main.py
@@ -9,21 +9,22 @@ load_dotenv()
 
 
 
+topic = "Analysis of Speech to Text"
 
 researcher = Agent(
     role="Expert Researcher",
-    goal="Uncover insights about the research paper",
-    backstory="""You work in Academia as a Lead Psychologist
-  Your goal is to understand papers in the field of psychology""",
+    goal=f"Find relevant and latest research related to the {topic}",
+    backstory="""You work in Academia as a Lead Machine Learning & AI Scientist
+  Your goal is to understand papers in the field of Artificial Intelligence and Machinelearning""",
     verbose=True,
     allow_delegation=False,
     tools=[query_tool],
     llm=chat_llm,
 )
 writer = Agent(
-    role="Expert Psychology Writer",
-    goal="Explain the experiment conducted in the article, focusing on the procedure and results obtained",
-    backstory="""You are a renowned Psychology Research Writer, known for your insightful and engaging articles.
+    role="Expert Machine Learning and AI  Writer",
+    goal=f"Explain the paper given and how exactly it is related to the {topic} at hand and give places where we can add it within our paper",
+    backstory="""You are a renowned Machine Learning & AI Writer, known for your insightful and engaging research papers.
   You transform complex concepts into compelling narratives.""",
     llm=chat_llm,
     verbose=True,
@@ -31,15 +32,15 @@ writer = Agent(
 )
 
 task1 = Task(
-    description="""Conduct a comprehensive analysis of the given research paper keeping in mind all the important points""",
+    description=f"""Find 10 to 20 relevant research papers related to the topic: {topic} and get the important information related to them Papers from the year 2023 till 2024""",
     expected_output="Complete analysis with all key points in bullet points",
     agent=researcher,
 )
 
 task2 = Task(
-    description="""Using the insights provided, Explain the experiment conducted in the article, focusing on the procedure and results obtained
+    description="""Using the insights provided, Explain How relevant to the topic are the research papers and how can they be used 
   Your post should be informative yet accessible and not missing any important detail""",
-    expected_output="Full analysis with explaining the experiment conducted in the article, focusing on the procedure and results obtained",
+    expected_output="Full analysis with explaining where in our research paper do the other papers fit in",
     agent=writer,
 )
 
